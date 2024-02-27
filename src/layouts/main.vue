6cc5ad49c80bc5a424efa1cf7d56052da3e8c731
Karthikeyan110803 <karthikeyan11803@gmail.com>
2024-02-27 16:32:42 +0530

    updated

--- src/layouts/main.vue
+++ src/layouts/main.vue
@@ -1,15 +1,15 @@
 <template>
-    <div class="q-pa-md">
-      <q-layout view="lHh lpr lFf">
-        <q-header elevated>
-          <q-toolbar class="glossy">
-            <q-btn flat round dense icon="menu" class="q-mr-sm" />
+    
+      <q-layout view="lHh lpr lFf ">
+        <q-header  class="bg-indigo-10">
+          <q-toolbar  >
+            <!-- <q-btn flat round dense icon="menu" class="q-mr-sm" /> -->
             <!-- <q-avatar>
               <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
             </q-avatar> -->
   
-            <q-toolbar-title>INFO TECH SOLUTIONS</q-toolbar-title>
-            <q-tabs>
+            <q-toolbar-title><SPAN class="text-red">EDEX</SPAN> TECH</q-toolbar-title>
+            <q-tabs class="desktop-only">
               <q-tab>HOME</q-tab>
               <q-tab>ABOUT US</q-tab>
               <q-tab>CONTACT US</q-tab>
@@ -30,6 +30,6 @@
           <RouterView/>
         </q-page-container>
       </q-layout>
-    </div>
+   
     
   </template>
